bb8922405c4bb095d41b5146cec7949afd826504
Borsh1992 <73963662+BorisLasker@users.noreply.github.com>
2022-07-06 14:17:25 +0300

    1

--- app/src/main/java/com/example/videosharing/Media.java
+++ app/src/main/java/com/example/videosharing/Media.java
@@ -2,15 +2,30 @@ package com.example.videosharing;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+import java.util.ArrayList;
+
 
 public class Media extends Fragment {
 
+
+    RecyclerView recyclerView;
+
+    private DatabaseReference myRef;
+
+    private ArrayList<Messages> messagesList;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -20,5 +35,36 @@ public class Media extends Fragment {
     }
 
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
+        recyclerView = view.findViewById(R.id.recyclerview);
+
+        LinearLayoutManager layoutManger = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(layoutManger);
+        recyclerView.setHasFixedSize(true);
+
+        myRef = FirebaseDatabase.getInstance().getReference();
+
+        messagesList = new ArrayList<>();
+
+        ClearALl();
+
+        GetDataFromFirebase();
+    }
+
+    private void GetDataFromFirebase() {
+
+
+    }
+
+    private void ClearALl(){
+        if(messagesList != null){
+            messagesList.clear();
+
+        }
+
+        messagesList = new ArrayList<>();
+    }
 }
---
+++ app/src/main/java/com/example/videosharing/Messages.java
@@ -0,0 +1,23 @@
+package com.example.videosharing;
+
+public class Messages {
+
+
+    String imageUrl;
+
+
+
+    public Messages(String imageUrl){
+
+        this.imageUrl = imageUrl;
+
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
+}
