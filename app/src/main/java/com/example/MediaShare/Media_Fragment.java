27e2053d23e9c3b48e95ad7fb9ec7853d5673afd
Borsh1992 <73963662+BorisLasker@users.noreply.github.com>
2022-07-13 15:43:58 +0300

    Update Media_Fragment.java

--- app/src/main/java/com/example/MediaShare/Media_Fragment.java
+++ app/src/main/java/com/example/MediaShare/Media_Fragment.java
@@ -4,20 +4,19 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
+
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
+
 import androidx.preference.PreferenceManager;
-import androidx.recyclerview.widget.DividerItemDecoration;
+
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.os.Parcelable;
+
 import android.util.Log;
 import android.view.LayoutInflater;
 
@@ -49,14 +48,12 @@ public class Media_Fragment extends Fragment {
     private DatabaseReference myRef;
 
     private ArrayList<MultiModel> messagesList;
-    private ArrayList<MultiModel>tempmessageList = new ArrayList<MultiModel>();
+    private  ArrayList<MultiModel>tempmessageList = new ArrayList<MultiModel>();
 
     private MultiAdapter recyclerAdapter;
-    private Context mContext;
+
 
     String email;
-    private int flag=0;
-
     public Media_Fragment() {
     }
 
