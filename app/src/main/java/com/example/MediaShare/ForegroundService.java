3ee318f582e82a65a223f7a4c05f95c7b1babbd1
dany0234 <dany0234@gmail.com>
2022-07-18 14:43:16 +0300

    1
    1

--- app/src/main/java/com/example/MediaShare/ForegroundService.java
+++ app/src/main/java/com/example/MediaShare/ForegroundService.java
@@ -11,20 +11,36 @@ import android.os.Build;
 import android.os.IBinder;
 import android.util.Log;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.app.NotificationCompat;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.core.Constants;
+
 public class ForegroundService extends Service {
+    private FirebaseDatabase firebaseDatabase;
+    private DatabaseReference databaseReference;
+
+    private DatabaseReference mSearchedLocationReference;
     public static final String CHANNEL_ID = "ForegroundServiceChannel";
     @Override
     public void onCreate() {
+
         super.onCreate();
+        firebaseDatabase = FirebaseDatabase.getInstance();
+        databaseReference = firebaseDatabase.getReference();
     }
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+
         String input = intent.getStringExtra("inputExtra");
         createNotificationChannel();
-        Intent notificationIntent = new Intent(this, LoginForm.class);
+        Intent notificationIntent = new Intent(this, Main.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this,
                 0, notificationIntent, 0);
         Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
@@ -33,9 +49,29 @@ public class ForegroundService extends Service {
                 .setSmallIcon(R.drawable.icon)
                 .setContentIntent(pendingIntent)
                 .build();
-        startForeground(1, notification);
+
         //do heavy work on a background thread
         //stopSelf();
+
+
+
+
+        databaseReference.child("message").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                Log.i("123456", String.valueOf(snapshot.getChildrenCount()));
+                
+
+                startForeground(1, notification);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
         return START_NOT_STICKY;
     }
     @Override
@@ -47,6 +83,7 @@ public class ForegroundService extends Service {
     public IBinder onBind(Intent intent) {
         return null;
     }
+
     private void createNotificationChannel() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
             NotificationChannel serviceChannel = new NotificationChannel(
