45ad4a4249fbe3f71be0e83d13f3a77c2da0e7d4
Borsh1992 <73963662+BorisLasker@users.noreply.github.com>
2022-07-18 14:52:04 +0300

    Update ForegroundService.java

--- app/src/main/java/com/example/MediaShare/ForegroundService.java
+++ app/src/main/java/com/example/MediaShare/ForegroundService.java
@@ -19,6 +19,7 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.database.core.Constants;
 
@@ -56,12 +57,14 @@ public class ForegroundService extends Service {
 
 
 
-        databaseReference.child("message").addListenerForSingleValueEvent(new ValueEventListener() {
+
+
+
+        databaseReference.child("message").addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot snapshot) {
 
                 Log.i("123456", String.valueOf(snapshot.getChildrenCount()));
-                
 
                 startForeground(1, notification);
             }
